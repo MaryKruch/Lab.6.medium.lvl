5af3275599761850838c1a14e08cc469b8645349
1 <1@DESKTOP-ACMQRLH>
2024-05-16 01:10:17 +0200

    Добавьте файлы проекта.

---
+++ Lab.6.medium.lvl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab._6.medium.lvl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Lab.6.medium.lvl/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab._6.medium.lvl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputTextBox.Text;
+            string[] numbers = input.Split(' ');
+
+            if (numbers.Length != 9)
+            {
+                resultTextBlock.Text = "Введите массив из 9 вещественных чисел через пробел.";
+                return;
+            }
+
+            double[] nums = new double[9];
+            for (int i = 0; i < 9; i++)
+            {
+                nums[i] = Convert.ToDouble(numbers[i]);
+            }
+
+            double[] greaterThanTen = nums.Where(n => Math.Abs(n) > 10).ToArray();
+
+            if (greaterThanTen.Length == 0)
+            {
+                resultTextBlock.Text = "Нет чисел с модулем больше 10";
+                return;
+            }
+
+            double average = greaterThanTen.Average();
+            resultTextBlock.Text = $"элементы исходного, которые по модулю больше 10: {average}";
+        }
+    }
+}
